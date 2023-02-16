0f887685453cfa7de252e6700d4236a6ea67bf64
Jachin Zhang <wrzzx52@gmail.com>
2023-02-16 11:45:03 +0000

    init

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 
 
-项目介绍和使用见 https://imciel.com/2021/03/23/build-openwrt-packages-with-docker-openwrt-sdk/
+参考1： https://imciel.com/2021/03/23/build-openwrt-packages-with-docker-openwrt-sdk/  
+
+参考2：https://github.com/xiaorouji/openwrt-passwall/discussions/1603
--- build.sh
+++ build.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 
-sudo apt-get update
-sudo apt-get install upx -y
-cp /usr/bin/upx staging_dir/host/bin
-cp /usr/bin/upx-ucl staging_dir/host/bin
+# sudo apt-get update
+# sudo apt-get install upx -y
+# cp /usr/bin/upx staging_dir/host/bin
+# cp /usr/bin/upx-ucl staging_dir/host/bin
 
 ./scripts/feeds update -a
 
-pushd feeds/packages/lang
-rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-21.02/lang/golang
-popd
+# pushd feeds/packages/lang
+# rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-21.02/lang/golang
+# popd
 
+#在SDK根目录，强制优先安装PWpackages中的 feeds 软件包
+./scripts/feeds install -a -f -p PWpackages
+#在SDK根目录，安装 feeds 中luci-app-passwall的软件包
 ./scripts/feeds install luci-app-passwall
 
 make defconfig
 
+# make download -j8 V=s && find dl -size -1024c -exec ls -l {} \; && make package/luci-app-passwall/{clean,compile} -j4
 make package/luci-app-passwall/{clean,compile} -j4
+
+#进入make menuconfig
+# make menuconfig
+# #进入全局设置，如下面所示，然后保存，退出
+# [*] Select all target specific packages by default                                                    
+# [*] Select all kernel module packages by default                                                      
+# [*] Select all userspace packages by default
+# [*] Cryptographically sign package lists (NEW) 
+
 make package/index
