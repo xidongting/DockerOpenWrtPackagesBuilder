2601c86d375bc7278584ee471a8407baaa3ad5e4
Jachin Zhang <wrzzx52@gmail.com>
2023-02-17 03:43:57 +0000

    change by internet memes (but not work...)

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
