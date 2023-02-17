675443333aae36b5e93888a5696c7f8e14af41c7
Jachin Zhang <wrzzx52@gmail.com>
2023-02-17 03:47:36 +0000

    damn

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ make defconfig
 # [*] Select all userspace packages by default
 # [*] Cryptographically sign package lists (NEW) 
 
-make download -j8 V=s && find dl -size -1024c -exec ls -l {} \; 
+# make download -j8 V=s && find dl -size -1024c -exec ls -l {} \; 
 make package/luci-app-passwall/{clean,compile} -j4
 
 make package/index
